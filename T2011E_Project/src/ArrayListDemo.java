5c0e59eed16c85430ef15208cae526d92e52be73
Linh31 <nguyentuanlinh311@gmail.com>
2021-05-20 15:52:55 +0700

    assign 6

--- T2011E_Project/src/ArrayListDemo.java
+++ T2011E_Project/src/ArrayListDemo.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListDemo {
 
-    public static int[] store(){
+    public  int[] store(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array that is to be created::");
         int size = sc.nextInt();
@@ -17,14 +16,7 @@ public class ArrayListDemo {
 
     }
 
-//    public ArrayList[] arrayList(){
-//        int[] arr = new ArrayList[];
-//        System.out.println("nhap vao 1 mang");
-//
-//        return  arr;
-//    }
 
-//
         int s;
     public int sumArray(int arr[]){
         s = 0;
@@ -56,18 +48,18 @@ public class ArrayListDemo {
         return qt;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array that is to be created::");
-        int size = sc.nextInt();
-        int[] myArray = new int[size];
-        System.out.println("Enter the elements of the array ::");
-
-        for(int i=0; i<size; i++) {
-            myArray[i] = sc.nextInt();
-        }
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the size of the array that is to be created::");
+//        int size = sc.nextInt();
+//        int[] myArray = new int[size];
+//        System.out.println("Enter the elements of the array ::");
+//
+//        for(int i=0; i<size; i++) {
+//            myArray[i] = sc.nextInt();
+//        }
         ArrayListDemo a = new ArrayListDemo();
 //        int myArray[] = a.store;
-//        int arr[] = a.store();
+        int myArray[] = a.store();
 
 //        System.out.println("Array created is :: "+ Arrays.toString(arr));
 
